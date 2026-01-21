fc32b684e735fac2270964ade24b3abca541ed2d
Eun Mi Kim <koreaeunmi@gmail.com>
2026-01-21 16:26:56 -0500

    Update ViewController.swift

--- M1TourOfXcode/ViewController.swift
+++ M1TourOfXcode/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBAction func resultButton(_ sender: Any) {
+        let name = inputTextField.text!
+        valueLabel.text = "Hello, \(name)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Commit from GitHub")
+        print("Commit2")
         // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            print("Hello, World!")
+        }
     }
 
 
